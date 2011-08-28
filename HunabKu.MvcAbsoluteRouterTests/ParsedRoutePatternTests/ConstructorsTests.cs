7a809ee3ca85dcbd799dea49161737f260b0a1b3
Fabio Maulo <fabiomaulo@gmail.com>
2011-08-28 16:55:14 -0300

    Parsedroute ctor null

---
+++ HunabKu.MvcAbsoluteRouterTests/ParsedRoutePatternTests/ConstructorsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace HunabKu.MvcAbsoluteRouterTests.ParsedRoutePatternTests
+{
+	public class ConstructorsTests
+	{
+		[Test]
+ 		public void WhenEmptyPatternThenThrows()
+		{
+			Executing.This(() => ParsedRoutePattern.Parse(null)).Should().Throw<ArgumentNullException>();
+		}
+	}
+
+	public class ParsedRoutePattern
+	{
+		private ParsedRoutePattern(string pattern)
+		{
+			if (pattern == null)
+			{
+				throw new ArgumentNullException("pattern");
+			}
+		}
+
+		public static ParsedRoutePattern Parse(string pattern)
+		{
+			return new ParsedRoutePattern(pattern);
+		}
+	}
+}
