e3fc73977a327438ecc867bc78b073bea76ada34
Fabio Maulo <fabiomaulo@gmail.com>
2011-09-18 14:50:39 -0300

    AbsoluteRoute ctor assign others values

--- HunabKu.MvcAbsoluteRouterTests/AbsoluteRouteTests/ConstructorsTests.cs
+++ HunabKu.MvcAbsoluteRouterTests/AbsoluteRouteTests/ConstructorsTests.cs
@@ -29,5 +29,29 @@ namespace HunabKu.MvcAbsoluteRouterTests.AbsoluteRouteTests
 			var route = new AbsoluteRoute(pattern, routeHandler:stopRoutingHandler);
 			route.RouteHandler.Should().Be(stopRoutingHandler);
 		}
+
+		[Test]
+		public void WhenUseDefaultsThenSetValue()
+		{
+			var value = new RouteValueDictionary();
+			var route = new AbsoluteRoute("{controller}/{action}/{id}", defaults: value);
+			route.Defaults.Should().Be.SameInstanceAs(value);
+		}
+
+		[Test]
+		public void WhenUseConstraintsThenSetValue()
+		{
+			var value = new RouteValueDictionary();
+			var route = new AbsoluteRoute("{controller}/{action}/{id}", constraints: value);
+			route.Constraints.Should().Be.SameInstanceAs(value);
+		}
+
+		[Test]
+		public void WhenUseDataTokensThenSetValue()
+		{
+			var value = new RouteValueDictionary();
+			var route = new AbsoluteRoute("{controller}/{action}/{id}", dataTokens: value);
+			route.DataTokens.Should().Be.SameInstanceAs(value);
+		}
 	}
 }
