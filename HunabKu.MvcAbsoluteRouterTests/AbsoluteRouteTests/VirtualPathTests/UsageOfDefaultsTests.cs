b8a3144f3b999322249431b538d45d97b7a0b386
Fabio Maulo <fabiomaulo@gmail.com>
2011-10-06 13:04:13 -0300

    New tests to fix (defaults and provided values)

---
+++ HunabKu.MvcAbsoluteRouterTests/AbsoluteRouteTests/VirtualPathTests/UsageOfDefaultsTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Routing;
+using HunabKu.MvcAbsoluteRouter;
+using NUnit.Framework;
+using SharpTestsEx;
+using SharpTestsEx.Mvc;
+
+namespace HunabKu.MvcAbsoluteRouterTests.AbsoluteRouteTests.VirtualPathTests
+{
+	public class UsageOfDefaultsTests
+	{
+		[Test]
+		public void WhenHostMatchAndRouteHasDefaultsThenCreateAbsolutePathWithoutNotNeededDefaults()
+		{
+			// when has default and values are not explicitly defined then does not use those values to create the URL
+			var context = "https://acme.com".AsUri().ToHttpContext();
+			var requestContext = new RequestContext(context, new RouteData());
+
+			var route = new AbsoluteRoute("http://{host}.com/{controller}/{action}", defaults: new RouteValueDictionary(new { controller = "Home", action = "Index" }));
+			var virtualPath = route.GetVirtualPath(requestContext, new RouteValueDictionary(new { host = "acme" }));
+			virtualPath.Route.Should().Be.SameInstanceAs(route);
+			virtualPath.VirtualPath.Should().Be("https://acme.com");
+		}
+
+		[Test]
+		public void WhenHostMatchAndRouteHasDefaultsAndLastValueThenCreateAbsolutePathWithDefaultsAndValues()
+		{
+			// when has default and some values are explicitly defined then use defaults and values to create the URL
+			var context = "https://acme.com".AsUri().ToHttpContext();
+			var requestContext = new RequestContext(context, new RouteData());
+
+			var route = new AbsoluteRoute("http://{host}.com/{controller}/{action}", defaults: new RouteValueDictionary(new { controller = "Home", action = "Index" }));
+			var virtualPath = route.GetVirtualPath(requestContext, new RouteValueDictionary(new { host = "acme", action="Edit" }));
+			virtualPath.Route.Should().Be.SameInstanceAs(route);
+			virtualPath.VirtualPath.Should().Be("https://acme.com/Home/Edit");
+		}
+
+		[Test]
+		public void WhenHostMatchAndRouteHasDefaultsAndFirstValueThenCreateAbsolutePathWithoutNotNeededDefaults()
+		{
+			// when has default and some values are explicitly defined then use defaults and values to create the URL
+			var context = "https://acme.com".AsUri().ToHttpContext();
+			var requestContext = new RequestContext(context, new RouteData());
+
+			var route = new AbsoluteRoute("http://{host}.com/{controller}/{action}", defaults: new RouteValueDictionary(new { controller = "Home", action = "Index" }));
+			var virtualPath = route.GetVirtualPath(requestContext, new RouteValueDictionary(new { host = "acme", controller = "Catalog" }));
+			virtualPath.Route.Should().Be.SameInstanceAs(route);
+			virtualPath.VirtualPath.Should().Be("https://acme.com/Catalog");
+		}
+	}
+}
