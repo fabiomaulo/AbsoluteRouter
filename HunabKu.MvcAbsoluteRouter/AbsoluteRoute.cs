4ca0729c1910a727fd906d5be2416fea94e42eb1
Fabio Maulo <fabiomaulo@gmail.com>
2011-10-06 15:48:40 -0300

    VirtualPath with querystring

--- HunabKu.MvcAbsoluteRouter/AbsoluteRoute.cs
+++ HunabKu.MvcAbsoluteRouter/AbsoluteRoute.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Routing;
@@ -135,6 +136,10 @@ namespace HunabKu.MvcAbsoluteRouter
 
 		public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values)
 		{
+			if (values == null)
+			{
+				values = new RouteValueDictionary();
+			}
 			var contextValues = new RouteValueDictionary(requestContext.RouteData.Values);
 			OverrideMergeDictionary(values, contextValues);
 			if (!MatchConstraints(contextValues))
@@ -147,13 +152,18 @@ namespace HunabKu.MvcAbsoluteRouter
 			{
 				defaultScheme = requestContext.HttpContext.Request.Url.Scheme;
 			}
-
-			IEnumerable<string> hostSegments = GetFullFilledSegments(parsedRoute.HostSegments, contextValues, Defaults, true);
-			IEnumerable<string> pathSegments = GetFullFilledSegments(parsedRoute.PathSegments, contextValues, Defaults);
+			HashSet<string> usedParametersNames = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+			IEnumerable<string> hostSegments = GetFullFilledSegments(parsedRoute.HostSegments, contextValues, Defaults, usedParametersNames, true);
+			IEnumerable<string> pathSegments = GetFullFilledSegments(parsedRoute.PathSegments, contextValues, Defaults, usedParametersNames);
+			string host = string.Join(".", hostSegments);
 			string path = string.Join("/", pathSegments);
+			
+			var parametersToUseInQuerystring = new HashSet<string>(values.Keys, StringComparer.OrdinalIgnoreCase);
+			parametersToUseInQuerystring.ExceptWith(usedParametersNames);
+			string queryString= GetQueryForUnusedParameters(values, parametersToUseInQuerystring);
 			string virtualPath = parsedRoute.HostSegments.Any()
-			                     	? (new UriBuilder {Scheme = defaultScheme, Host = string.Join(".", hostSegments), Path = path}).ToString()
-			                     	: path;
+			                     	? (new UriBuilder {Scheme = defaultScheme, Host = host, Path = path, Query = queryString.TrimStart('?')}).ToString()
+			                     	: path + queryString;
 
 			var virtualPathData = new VirtualPathData(this, virtualPath);
 			OverrideMergeDictionary(DataTokens, virtualPathData.DataTokens);
@@ -161,7 +171,30 @@ namespace HunabKu.MvcAbsoluteRouter
 			return virtualPathData;
 		}
 
-		private IEnumerable<string> GetFullFilledSegments(IEnumerable<string> patternSegments, RouteValueDictionary values, RouteValueDictionary defaults, bool forceUsageOfDefaultWhereNoValueAvailable = false)
+		private string GetQueryForUnusedParameters(RouteValueDictionary values, HashSet<string> parametersToUseInQuerystring)
+		{
+			if (parametersToUseInQuerystring.Count > 0)
+			{
+				var queryStringBuilder = new StringBuilder(512);
+				bool firstParam = true;
+				foreach (string unusedNewValue in parametersToUseInQuerystring)
+				{
+					object value;
+					if (values.TryGetValue(unusedNewValue, out value))
+					{
+						queryStringBuilder.Append(firstParam ? '?' : '&');
+						firstParam = false;
+						queryStringBuilder.Append(Uri.EscapeDataString(unusedNewValue));
+						queryStringBuilder.Append('=');
+						queryStringBuilder.Append(Uri.EscapeDataString(Convert.ToString(value, CultureInfo.InvariantCulture)));
+					}
+				}
+				return queryStringBuilder.ToString();
+			}
+			return string.Empty;
+		}
+
+		private IEnumerable<string> GetFullFilledSegments(IEnumerable<string> patternSegments, RouteValueDictionary values, RouteValueDictionary defaults, HashSet<string> usedParametersNames, bool forceUsageOfDefaultWhereNoValueAvailable = false)
 		{
 			List<string> pendingSubstitutions = new List<string>(10);
 			var availableValues = new RouteValueDictionary(values);
@@ -186,11 +219,13 @@ namespace HunabKu.MvcAbsoluteRouter
 							}
 							pendingSubstitutions.Clear();
 						}
+						usedParametersNames.Add(variableName);
 						var actualValueString = Convert.ToString(actualValue, CultureInfo.InvariantCulture);
 						yield return actualValueString;
 					}
 					else if (defaults.TryGetValue(variableName, out actualValue))
 					{
+						usedParametersNames.Add(variableName);
 						// enlist the availability of a default
 						pendingSubstitutions.Add(Convert.ToString(actualValue, CultureInfo.InvariantCulture));
 					}
---
+++ HunabKu.MvcAbsoluteRouterTests/AbsoluteRouteTests/VirtualPathTests/QueryStringTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Web.Routing;
+using HunabKu.MvcAbsoluteRouter;
+using NUnit.Framework;
+using SharpTestsEx;
+using SharpTestsEx.Mvc;
+
+namespace HunabKu.MvcAbsoluteRouterTests.AbsoluteRouteTests.VirtualPathTests
+{
+	public class QueryStringTests
+	{
+		[Test]
+		public void WhenPathMatchAndHasAdditionalValueThenCreateRelativePathWithQueryString()
+		{
+			var context = "http://acme.com".AsUri().ToHttpContext();
+			var requestContext = new RequestContext(context, new RouteData());
+
+			var route = new AbsoluteRoute("{controller}/{action}");
+			var virtualPath = route.GetVirtualPath(requestContext, new RouteValueDictionary(new { controller = "pizza", action = "calda", currentPage = 2 }));
+			virtualPath.Route.Should().Be.SameInstanceAs(route);
+			virtualPath.VirtualPath.Should().Be("pizza/calda?currentPage=2");
+		}
+
+		[Test]
+		public void WhenPathMatchAndHasAdditionalValuesThenCreateRelativePathWithQueryString()
+		{
+			var context = "http://acme.com".AsUri().ToHttpContext();
+			var requestContext = new RequestContext(context, new RouteData());
+
+			var route = new AbsoluteRoute("{controller}/{action}");
+			var virtualPath = route.GetVirtualPath(requestContext, new RouteValueDictionary(new { controller = "pizza", action = "calda", topLevelDomain = "xx", currentPage = 2 }));
+			virtualPath.Route.Should().Be.SameInstanceAs(route);
+			virtualPath.VirtualPath.Should().Be("pizza/calda?topLevelDomain=xx&currentPage=2");
+		}
+
+
+		[Test]
+		public void WhenHostMatchThenCreateAbsolutePathWithSchemeFromContext()
+		{
+			var context = "https://acme.com".AsUri().ToHttpContext();
+			var requestContext = new RequestContext(context, new RouteData());
+
+			var route = new AbsoluteRoute("http://{host}.com/{controller}/{action}", defaults: new RouteValueDictionary(new { controller = "Home", action="Index" }));
+			var virtualPath = route.GetVirtualPath(requestContext, new RouteValueDictionary(new { host = "acme", page = 10 }));
+			virtualPath.Route.Should().Be.SameInstanceAs(route);
+			virtualPath.VirtualPath.Should().Be("https://acme.com/?page=10");
+		}
+
+	}
+}
