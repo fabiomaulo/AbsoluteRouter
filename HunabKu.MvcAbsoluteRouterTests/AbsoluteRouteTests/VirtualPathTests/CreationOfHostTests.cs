e16ac810c24172e48b6b56d08462c2c6d4d9fe39
Fabio Maulo <fabiomaulo@gmail.com>
2011-10-06 18:56:21 -0300

    Failing tests for Host with catchall

---
+++ HunabKu.MvcAbsoluteRouterTests/AbsoluteRouteTests/VirtualPathTests/CreationOfHostTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Routing;
+using HunabKu.MvcAbsoluteRouter;
+using NUnit.Framework;
+using SharpTestsEx;
+using SharpTestsEx.Mvc;
+
+namespace HunabKu.MvcAbsoluteRouterTests.AbsoluteRouteTests.VirtualPathTests
+{
+	public class CreationOfHostTests
+	{
+		[Test]
+		public void WhenHostHasCatchAllWithNoValueThenUseContextAsDefault()
+		{
+			var context = "http://ar.acme.com/pizza".AsUri().ToHttpContext();
+			var requestContext = new RequestContext(context, new RouteData());
+
+			var route = new AbsoluteRoute("{topLevelDomain}.{*domain}");
+			var virtualPath = route.GetVirtualPath(requestContext, new RouteValueDictionary(new { topLevelDomain = "cl" }));
+			virtualPath.VirtualPath.Should().Be("http://cl.acme.com/");
+		}
+
+		[Test]
+		public void WhenHostHasCatchAllWithValueThenUseValue()
+		{
+			var context = "http://ar.acme.com/pizza".AsUri().ToHttpContext();
+			var requestContext = new RequestContext(context, new RouteData());
+
+			var route = new AbsoluteRoute("{topLevelDomain}.{*domain}");
+			var virtualPath = route.GetVirtualPath(requestContext, new RouteValueDictionary(new { topLevelDomain = "cl", domain="google.net" }));
+			virtualPath.VirtualPath.Should().Be("http://cl.google.net/");
+		}
+	}
+}
